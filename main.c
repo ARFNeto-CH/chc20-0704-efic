3342989102c9478d0a82c0dff4739849d929fd59
ARFNeto-CH <arfneto@hotmail.com>
2020-07-04 16:17:08 -0300

    formato

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "ref.h"
 int main()
-{	char    palavra[30], * p = palavra;
+{	char    palavra[30], *p = palavra;
 	char    c = 0;
 	int     n = 0; // letras na palavra lida
 	while ((c = fgetc(stdin)) != '\n')
-	{
-		n++; // leu mais uma letra
+	{	n++; // leu mais uma letra
 		if (idx[c]) *p++ = c; // guarda a vogal
 	};	// while()
 	*p = 0; // termina a string
